3f4af755474d15d282a33c0a253e4328080201cc
buregeyaleonard1 <buregeyaleonard1@gmail.com>
2026-04-25 21:20:49 +0200

    Add toggleMenu function to manage menu visibility

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu");
 
+  if (menu.style.left === "0px") {
+    menu.style.left = "-250px";
+  } else {
+    menu.style.left = "0px";
+  }
+}
